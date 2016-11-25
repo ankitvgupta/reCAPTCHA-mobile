2761dc328b33804b9c3d2b1db758ee502a6f62f8
Ezra <ezrazigmond@gmail.com>
2016-11-24 22:50:36 -0500

    Registrations are now on a per-user basis, limited per user

--- captcha-server/app.py
+++ captcha-server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_oauth import OAuth
 
 from Crypto.Random import random
@@ -8,6 +8,7 @@ from urllib2 import Request, urlopen, URLError
 
 KEYLENGTH = 40
 MAXDOMAINS = 10
+MAXPERUSER = 5
 
 with open('secrets.json') as secrets:
     data = json.loads(secrets.read())
@@ -17,12 +18,15 @@ with open('secrets.json') as secrets:
 
 REDIRECT_URI = '/oauth2callback'
 
+SITES_DB = 'sites.db'
+USERS_DB = 'users.db'
+
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
 oauth = OAuth()
 google = oauth.remote_app('google',
-                          base_url='https://www.google.com/accounts/',
+                          base_url='https://www.googleapis.com/oauth2/v1/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
                           request_token_url=None,
                           request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
@@ -33,26 +37,14 @@ google = oauth.remote_app('google',
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
 
-@app.route('/register')
-def register():
-    login_token = session.get('login_token')
-    print login_token
-    if login_token is None:
-        return redirect(url_for('login'))
+@app.route('/')
+def clear():
+    session.clear()
+    return "OK"
 
-    headers = {'Authorization': 'OAuth ' + login_token[0]}
-    req = Request('https://www.googleapis.com/oauth2/v1/userinfo', None, headers)
-
-    try:
-        res = urlopen(req)
-    except URLError, e:
-        print e
-        if e.code == 401:
-            # Bad token
-            session.pop('access_token', None)
-            return redirect(url_for('login'))
-
-    return render_template('register.html')
+@app.route('/error')
+def error():
+    return "Something went wrong :("
 
 @app.route('/login')
 def login():
@@ -62,16 +54,46 @@ def login():
 @app.route(REDIRECT_URI)
 @google.authorized_handler
 def authorized(oauth_resp):
+    if oauth_resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+
     login_token = oauth_resp['access_token']
     session['login_token'] = login_token, ''
-    return redirect(url_for('register'))
+
+    resp = google.get('userinfo')
+    if resp.status != 200:
+        session.pop('login_token', None)
+        return redirect(url_for('error'))
+    else: 
+        session['email'] = resp.data['email']
+        return redirect(url_for('register'))
 
 @google.tokengetter
 def get_login_token():
     return session.get('login_token')
 
+
+@app.route('/register')
+def register():
+    if 'login_token' not in session or 'email' not in session:
+        return redirect(url_for('login'))
+
+    db = pickledb.load(USERS_DB, False)
+    registrations = db.get(session['email'])
+
+    return render_template('register.html', 
+                            session=session,
+                            registrations=registrations,
+                            maxperuser=MAXPERUSER)
+
 @app.route('/new_registration', methods=['POST'])
 def new_registration():
+    if 'login_token' not in session or 'email' not in session:
+        return redirect(url_for('login'))
+
     def randomString(length):
         import string
         charset = string.ascii_letters + string.digits + '-_!'
@@ -90,15 +112,33 @@ def new_registration():
     if len(domains) == 0:
         return "Must specify at least one domain", 400
 
-    domains = [s.strip() for s in domains.split('\n')]
+    domains = [s for s in [s.strip() for s in domains.split('\n')] if len(s) != 0]
     if len(domains) > MAXDOMAINS:
         return "Specified too many domains", 400
 
-    db = pickledb.load('sites.db', False)
-    pair = newKeyPair(db)
+    label = request.form['label']
+    if len(label) == 0:
+        return "Must specify a label", 400
 
-    db.set(pair[0], (pair[1], domains))
-    db.dump()
+    email = session['email']
+    user_db = pickledb.load(USERS_DB, False)
+    site_db = pickledb.load(SITES_DB, False)
+
+    registrations = user_db.get(email)
+    if (len(registrations) >= MAXPERUSER):
+        return "Signed up for max keys", 403
+
+    pair = newKeyPair(site_db)
+
+    site_db.set(pair[0], (str(label), pair[1], domains))
+    site_db.dump()
+
+
+    if registrations == None:
+        user_db.set(email, [(str(label), pair[0], pair[1], domains)])
+    else:
+         user_db.set(email, registrations + [(str(label), pair[0], pair[1], domains)])
+    user_db.dump()
 
     response = json.dumps({'siteID': pair[0], 'secret': pair[1]})
 
